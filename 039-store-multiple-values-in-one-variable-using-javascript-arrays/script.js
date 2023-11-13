17ef270c556aeb2d54873caa39b02a50d1a7afab
nedoratic <nedoratic@gmail.com>
2023-11-13 19:42:40 +0100

    arrays, access array data, modify array data, push, pop, shift and unshif array methods

---
+++ 039-store-multiple-values-in-one-variable-using-javascript-arrays/script.js
@@ -0,0 +1,17 @@
+// With JavaScript array variables, we can store several pieces of data in one place.
+
+// You start an array declaration with an opening square bracket, end it with a closing square bracket, and put a comma between each entry, like this:
+
+const sandwich = ["peanut butter", "jelly", "bread"];
+
+console.log(sandwich);
+
+// TASK
+
+// Modify the new array myArray so that it contains both a string and a number (in that order).
+
+// Only change code below this line
+
+const myArray = ["It was friday when the rest of my life started!", 20];
+
+console.log(myArray);
---
+++ 040-nest-one-array-within-another-array/script.js
@@ -0,0 +1,24 @@
+// You can also nest arrays within other arrays, like below:
+
+const teams = [
+	["Bulls", 23],
+	["White Sox", 45],
+];
+
+console.log(teams);
+
+// This is also called a multi-dimensional array.
+
+// TASK
+
+// Create a nested array called myArray.
+
+// Only change code below this line
+
+const myArray = [
+	["Konstantina", "December", 5, 2020],
+	["Dijana", "August", 23, 1993],
+	["Nedo", "January", 20, 1987],
+];
+
+console.log(myArray);
