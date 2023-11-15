05d926af975e3a869d38187f057bf91c3615b2ef
nedoratic <nedoratic@gmail.com>
2023-11-15 18:12:11 +0100

    if, esle if and else statements

---
+++ 071-introducing-else-statements/script.js
@@ -0,0 +1,23 @@
+// When a condition for an if statement is true, the block of code following it is executed. What about when that condition is false? Normally nothing would happen. With an else statement, an alternate block of code can be executed.
+
+// if (num > 10) {
+// 	return "Bigger than 10";
+// } else {
+// 	return "10 or Less";
+// }
+
+// TASK
+
+// Combine the if statements into a single if/else statement.
+
+function testElse(val) {
+	let result = "";
+	if (val > 5) {
+		result = "Bigger than 5";
+	} else {
+		result = "5 or Smaller";
+	}
+	return result;
+}
+
+console.log(testElse(4));
